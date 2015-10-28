77999cff1572c7324427a89ecf9a3a6fb53fbb2f
Leah Klamkin <leahcodes@gmail.com>
2015-10-28 13:59:36 -0700

    Basic CRUD for Eat model.

--- app/controllers/eats_controller.rb
+++ app/controllers/eats_controller.rb
@@ -3,6 +3,10 @@ class EatsController < ApplicationController
     @eats = Eat.all
   end
 
+  def show
+    @eat = Eat.find(params[:id])
+  end
+
   def new
     @eat = Eat.new
   end
@@ -16,6 +20,28 @@ class EatsController < ApplicationController
       flash[:alert] = "Whoopsies, there was an error somewhere!"
       redirect_to new_eat_path
     end
+  end
+
+  def edit
+    @eat = Eat.find(params[:id])
+  end
+
+  def update
+    @eat = Eat.find(params[:id])
+    if @eat.update(eat_params)
+      flash[:notice] = "Changes Saved!"
+      redirect_to eat_path(@eat)
+    else
+      flash[:alert] = "Whoopsies, there was an error somewhere!"
+      redirect_to edit_eat_path(@eat)
+    end
+  end
+
+  def destroy
+    @eat = Eat.find(params[:id])
+    @eat.destroy
+    flash[:notice] = "Be gone with ye'!"
+    redirect_to eats_path
   end
 
   private
